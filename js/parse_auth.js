3d50f7ed44a49643c9f24eaaf01146a717f54d69
Josh Brzinski <jbbrzinski@gmail.com>
2015-11-04 09:40:26 -0500

    IDK LOL WTF

--- js/parse_auth.js
+++ js/parse_auth.js
@@ -1,4 +1,4 @@
- const APP_ID = 'PZ6RarOP9msJDLa5elO6MxoWTCaY1hqBQk5jTlCr';
+const APP_ID = 'PZ6RarOP9msJDLa5elO6MxoWTCaY1hqBQk5jTlCr';
 const API_KEY = 'C5qXP7XXHa6qZCLolbEJixgfcsKcn80LfKq0m6xR';
 const APP_URL = 'https://api.parse.com/1/classes/Todos';
 
--- js/views/MainView.js
+++ js/views/MainView.js
@@ -5,21 +5,23 @@ export default React.createClass({
 
   render(){
     return(
-      <header>
-        <h1>Things Todo</h1>
-      </header>
+      <div>
+        <header>
+          <h1>Things Todo</h1>
+        </header>
         <main>
           <form onSubmit={this.props.onSubmitHandler} class="todo-add">
-            <input type="text" name="title" placeholder="Add Something">
+            <input type="text" name="title" placeholder="Add Something"></input>
             <button><i class="fa fa-plus"></i></button>
           </form>
           <ul class="todo-list">
             items={todos.toJSON()}
           </ul>
         </main>
-      <footer>
-        <button class="clear">Clear Complete</button>
-      </footer>
+        <footer>
+          <button class="clear">Clear Complete</button>
+        </footer>
+      </div>
     );
   }
 
